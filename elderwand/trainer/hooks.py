37c647d50a0ba111ee49c5d3c948792b97ce932e
Dariush Bahrami <dariushbahrami1993@gmail.com>
2022-08-12 23:37:29 +0430

    docs: :memo: add docs

--- elderwand/trainer/hooks.py
+++ elderwand/trainer/hooks.py
@@ -1,7 +1,10 @@
-import math
-from typing import Callable, NamedTuple, Sequence
+"""Training hooks
 
-from scipy.stats import logistic
+In this module, the common training hooks are defined.
+"""
+import math
+from typing import Callable, NamedTuple
+
 from tqdm import tqdm
 
 
@@ -13,26 +16,53 @@ class TrainingMetrics(NamedTuple):
 
 
 class DiscriminatorSigmoidApplierHook:
+    """Apply sigmoid function to discriminator output
+
+    When using BCEWithLogitsLoss the output of the discriminator is not outputed from
+    the sigmoid function. This hook is used to apply the sigmoid function to the output
+    of the discriminator.
+    """
+
     def __init__(self, epsilon: float = 1e-8):
-        self.metrics_to_apply = [
+        """Class constructor
+
+        Args:
+            epsilon (float, optional): A small number to avoid division by zero.
+                Defaults to 1e-8.
+        """
+        self._metrics_to_apply: list[str] = [
             "discriminator_output_on_real_data",
             "discriminator_output_on_fake_data",
         ]
-        self.sigmoid = lambda x: 1 / (1 + math.exp(-x) + epsilon)
+        self._sigmoid: Callable = lambda x: 1 / (1 + math.exp(-x) + epsilon)
 
     def __call__(self, mini_batch_metrics: TrainingMetrics) -> TrainingMetrics:
+        """Apply sigmoid function to discriminator output
+
+        Args:
+            mini_batch_metrics (TrainingMetrics): Metrics of the current mini-batch.
+
+        Returns:
+            TrainingMetrics: Updated metrics of the current mini-batch.
+        """
         updated_metrics = {}
         for key, value in mini_batch_metrics._asdict().items():
-            if key not in self.metrics_to_apply:
+            if key not in self._metrics_to_apply:
                 updated_metrics[key] = value
             else:
-                updated_metrics[key] = self.sigmoid(value)
+                updated_metrics[key] = self._sigmoid(value)
         return TrainingMetrics(**updated_metrics)
 
 
 class MetricsAggregatorHook:
+    """Store metrics during training
+
+    Attributes:
+        metrics (dict): Dictionary of metrics.
+    """
+
     def __init__(self):
-        self.metrics = {
+        self.metrics: dict[str, list[float]] = {
             "discriminator_output_on_real_data": [],
             "discriminator_output_on_fake_data": [],
             "discriminator_loss": [],
@@ -40,21 +70,48 @@ class MetricsAggregatorHook:
         }
 
     def __call__(self, mini_batch_metrics: TrainingMetrics) -> TrainingMetrics:
+        """Store metrics and return them unchanged
+
+        Args:
+            mini_batch_metrics (TrainingMetrics): Metrics of the current mini-batch.
+
+        Returns:
+            TrainingMetrics: Unchanged metrics of the current mini-batch.
+        """
         for key, value in mini_batch_metrics._asdict().items():
             self.metrics[key].append(value)
         return mini_batch_metrics
 
 
 class TQDMHook:
+    """A hook that updates the progress bar during training.
+
+    Attributes:
+        progress_bar (tqdm): Progress bar to update.
+    """
+
     def __init__(self, progress_bar: tqdm):
-        self.iteration = 0
+        """Class constructor
+
+        Args:
+            progress_bar (tqdm): Progress bar to update.
+        """
         self.progress_bar = progress_bar
-        self.name_mapper = {
+        self._iteration = 0
+        self._name_mapper = {
             "discriminator_output_on_real_data": "D(x)",
             "discriminator_output_on_fake_data": "D(G(z))",
         }
 
     def __call__(self, mini_batch_metrics: TrainingMetrics) -> TrainingMetrics:
+        """Update progress bar and return unchanged metrics
+
+        Args:
+            mini_batch_metrics (TrainingMetrics): Metrics of the current mini-batch.
+
+        Returns:
+            TrainingMetrics: Unchanged metrics of the current mini-batch.
+        """
         self.iteration += 1
         postfix = []
         for key in self.name_mapper:
@@ -66,9 +123,34 @@ class TQDMHook:
 
 
 class ChainedHooks:
-    def __init__(self, *hooks: Callable[[TrainingMetrics], None]):
-        self.hooks: Sequence[str, Callable[[TrainingMetrics], None]] = hooks
+    """Chain multiple hooks
+
+    This hook is used to chain multiple hooks together and call them in sequence. The
+    hooks are called in the order they are passed to the constructor. The output of the
+    each hook is passed to the next hook in the sequence and the output of the last hook
+    is returned.
+
+    Attributes:
+        hooks (tuple): Hooks to chain.
+    """
+
+    def __init__(self, *hooks: Callable[[TrainingMetrics], TrainingMetrics]):
+        """Class constructor
+
+        Args:
+            *hooks (Callable[[TrainingMetrics], TrainingMetrics]): Hooks to chain.
+        """
+        self.hooks: tuple[Callable[[TrainingMetrics], TrainingMetrics]] = hooks
 
     def __call__(self, mini_batch_metrics: TrainingMetrics) -> TrainingMetrics:
+        """Call all hooks in sequence and return the output of the last hook
+
+        Args:
+            mini_batch_metrics (TrainingMetrics): Metrics of the current mini-batch.
+
+        Returns:
+            TrainingMetrics: Output of the last hook.
+        """
         for hook in self.hooks:
             mini_batch_metrics = hook(mini_batch_metrics)
+        return mini_batch_metrics
