58d9a28c9f2784ae6b3a9dd8e28a3048313d62e0
Dariush Bahrami <dariushbahrami1993@gmail.com>
2022-08-14 01:27:10 +0430

    fix: update DCGAN weight init based on pytorch official tutorial

--- elderwand/models/dcgan/shared.py
+++ elderwand/models/dcgan/shared.py
@@ -5,5 +5,5 @@ def dcgan_weights_init(module):
     if isinstance(module, (nn.Conv2d, nn.ConvTranspose2d)):
         nn.init.normal_(module.weight.data, 0.0, 0.02)
     elif isinstance(module, nn.BatchNorm2d):
-        nn.init.normal_(module.weight.data, 0.0, 0.02)
+        nn.init.normal_(module.weight.data, 1.0, 0.02)
         nn.init.constant_(module.bias.data, 0)
